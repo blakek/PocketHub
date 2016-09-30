57d6bcc22ea2d642c791a8d37097bb06f6ab0ebf
Blake Knight <bknight@gsandf.com>
2016-09-30 08:54:46 -0500

    add vector icons and a UI kit
    added react-native-vector-icons for icons and react-native-elements for
    other UI elements

---
+++ components/SignInView/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { SocialIcon } from 'react-native-elements'
+
+export const SignInView = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>
+      PocketHub
+    </Text>
+
+    <Text style={styles.instructions}>
+      To get started, sign in using your GitHub account
+    </Text>
+
+    <SocialIcon
+      title='Sign In With GitHub'
+      button
+      type='github'
+      style={{
+        paddingLeft: 10,
+        paddingRight: 10
+      }}
+    />
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f3fcff'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333',
+    marginBottom: 25
+  }
+})
+
+export default SignInView
